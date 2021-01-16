161c8f537f0b188cbbeb7afe8261c30d8ca76ce2
mPerier <=>
2021-01-17 00:05:19 +0100

    Added the autocalibration process, more infos in the .xlsx file. Functional on testBench, tests to do

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
 // ------------------- DEFINES ----------------------
 #define inWire1 A0
 #define inWire2 A1
 #define outWire 10
+#define confgBP 4
+#define led     5
 
-#define seuil_min 1.5 // The voltage under which no button is pressed
+#define seuil_min_V 1.5 // The voltage under which no button is pressed
 
 #define trig_inRes_Ohm 470 // The trigger resistance and voltage
 #define in_supplyVoltage 5
@@ -13,18 +16,23 @@
 #define out_inRes_Ohm 1000 // The output resistance and voltage
 #define out_supplyVoltage 12 
 
+#define tolerance_V 0.1
+
+#define blinkDuration_ms 1000
+
+enum {VOLM, VOLP, NEXT, PREV, FNC1, FNC2, FNC3, FNC4};
 
 // --------------------- DATA -----------------------
 // Represents the resistance of each button
 // WIRE1: VOL- | VOL + | PREV | NEXT
 // WIRE2: MEM- | MEM+  | MUTE | MODE
-const unsigned int triggers_ohm[4] = {10,57,125,285};
+float triggers_V[8];
+bool trig_Wires[8];
 
 // Because the readed voltage is inversly proportional to the trigger resistance,
 //  The loops that will use triggers_V will go from the last element to the first one.
-float triggers_V[4];
 
-const float actions_V[8] = {
+float actions_V[8] = {
               // _______________________
               // | OUT_K | FUNC | IN_R |
               // |_____________________|
@@ -41,22 +49,48 @@ const float actions_V[8] = {
 // ------------------ PROTOTYPES --------------------
 
 void sendPulse(float level_V);
+void autoCal(void);
+void blink(int nbBlink);
+int getRmtCmd(void);
+void writeConfig(void);
+unsigned char toByte(bool b[8]);
+void fromByte(unsigned char c, bool b[8]);
 
 
 // -------------------- SETUP -----------------------
 void setup() {
-  int i;
   pinMode(inWire1,INPUT);
   pinMode(inWire2,INPUT);
   pinMode(outWire,OUTPUT);
+  pinMode(confgBP,INPUT);
+  pinMode(led,    OUTPUT);
 
   Serial.begin(9600);
 
-  // Init Triggers_V
-  Serial.println("TRIGGERS:");
-  for (i=0;i<4;i++) {
-    triggers_V[i] = (float)in_supplyVoltage * ((float)trig_inRes_Ohm / ((float)triggers_ohm[i] + (float)trig_inRes_Ohm));
-    Serial.println(triggers_V[i]);
+  int i;
+  int magicWord;
+  float retreivedVoltage;
+
+  // If the user maintains the confgBP during 1s at startup, the autocal process will be launched
+  if (digitalRead(confgBP)){
+    delay(1000);
+    if (digitalRead(confgBP)){
+      EEPROM.put(0,0);
+    } 
+  }
+  EEPROM.get(0,magicWord);
+  if (magicWord != 101){
+    autoCal();
+  }
+  else {
+    Serial.println("magic word retreived, skipping autocal");
+    // Voltages
+    for (i=0;i<8;i++){
+      EEPROM.get(2+i*4,retreivedVoltage);
+      triggers_V[i] = retreivedVoltage;
+    }
+    // Wires
+    fromByte(EEPROM.read(34),trig_Wires);
   }
 }
 
@@ -64,7 +98,6 @@ void setup() {
 void loop() {
   int i;
   float lastVoltage;
-  char msg[50];
 
   float readedVoltage1 = (analogRead(inWire1)/(float)1024)*5;
   float readedVoltage2 = (analogRead(inWire2)/(float)1024)*5;
@@ -72,7 +105,7 @@ void loop() {
   bool triggered = false;
   
   // Wire1 Actions
-  lastVoltage = seuil_min;
+  lastVoltage = seuil_min_V;
   for (i=3;i>=0;i--){
     // If the readed voltage is inside one of the ranges
     if (readedVoltage1 > lastVoltage && readedVoltage1 < triggers_V[i]){
@@ -86,7 +119,7 @@ void loop() {
   
   // Wire2 Actions only if Wire1 haven't raised a trigger
   if (triggered == false){
-    lastVoltage = seuil_min;
+    lastVoltage = seuil_min_V;
     for (i=3;i>=0;i--){
       if (readedVoltage2 > lastVoltage && readedVoltage2 < triggers_V[i]){
         sendPulse(actions_V[i+4]);
@@ -99,11 +132,151 @@ void loop() {
 
 // ------------------ FUNCTIONS --------------------
 void sendPulse(float level_V){
+  Serial.println("Sending " + String(level_V) + "Volts");
   // The output is set to the given value (8 bit resolution)
   analogWrite(outWire,((level_V/(float)5)*(float)1024)/4);
   // While the button is maintained, the output stays high
-  while (analogRead(inWire1) > seuil_min || analogRead(inWire1) > seuil_min){
+  while (analogRead(inWire1) > seuil_min_V || analogRead(inWire1) > seuil_min_V){
     delay(50);
   }
   digitalWrite(outWire,0);
 }
+
+void blink(int nbBlink){
+  int i;
+  for (i=0;i<=nbBlink;i++){
+    delay((blinkDuration_ms/nbBlink)/2);
+    digitalWrite(led,true);
+    delay((blinkDuration_ms/nbBlink)/2);
+    digitalWrite(led,false);
+  }
+}
+
+void autoCal(){
+  int i;
+  bool cmdFound = false;
+  float wire1Read;
+  float wire2Read;
+
+  Serial.println("---1- Initialisation des boutons du volant -1--- ");
+
+  for (i=0;i<8;i++){
+
+    while (cmdFound == false) {
+      wire1Read = ((float)analogRead(inWire1)/(float)1024)*5;
+      wire2Read = ((float)analogRead(inWire2)/(float)1024)*5;
+
+      if (wire1Read >= seuil_min_V && digitalRead(confgBP)) {
+        triggers_V[i] = wire1Read;
+        trig_Wires[i] = false;
+        cmdFound = true;
+      }
+      else if (wire2Read >= seuil_min_V && digitalRead(confgBP)){
+        triggers_V[i] = wire2Read;
+        trig_Wires[i] = true;
+        cmdFound = true;
+      }
+      else {
+        blink(8);
+      }
+    }
+    Serial.println("Button ID " +String(i)+ " | Done: " +String(triggers_V[i])+ "V | Wire n " + String(trig_Wires[i]));
+    blink(1);
+    cmdFound = false;
+    delay(1500);
+  }
+
+  Serial.println("---2- Init volant OK, initialisation des fonctions -2--- ");  
+
+  cmdFound = false;
+  float voltage = 2.5;
+  for (i=0;i<8;i++){
+    while (!cmdFound) {
+      switch (getRmtCmd()) {
+        case VOLM:
+          voltage-=0.02;
+        break;
+        case VOLP:
+          voltage+=0.02;
+        break;
+        case NEXT:
+          voltage-=0.1;
+        break;
+        case PREV:
+          voltage+=0.1;
+        break;
+        case FNC1:
+          sendPulse(voltage);
+        break;
+        case FNC2:
+          cmdFound = true;
+        break;
+      }
+    blink(1);
+    Serial.println("[autoCal] Waiting for command...");
+    }
+
+    Serial.println("Action ID ["+String(i)+"] Voltage: "+String(voltage)+"V");
+    actions_V[i] = voltage;
+    cmdFound = false;
+  }
+
+  writeConfig();
+}
+
+int getRmtCmd(){
+  int i;
+  bool cmdFound = false;
+  
+  float wire1Read;
+  float wire2Read;
+
+  float min;
+  float max;
+
+  while (!cmdFound){
+    wire1Read = ((float)analogRead(inWire1)/(float)1024)*(float)5;
+    wire2Read = ((float)analogRead(inWire2)/(float)1024)*(float)5;
+
+    for (i=0;i<8;i++){
+      min = triggers_V[i] - tolerance_V;
+      max = triggers_V[i] + tolerance_V;
+
+      if ((trig_Wires[i] == 0 && wire1Read > min && wire1Read < max) || (trig_Wires[i] == 1 && wire2Read > min && wire2Read < max)) {
+        cmdFound = true;
+        break;
+      } 
+    }
+  }
+  Serial.println("[getRmtCmd]: Action ID: " + String(i));
+  return i;
+}
+
+void writeConfig(void){
+  int i;
+  // Voltages
+  for (i=0;i<8;i++){
+    EEPROM.put(2+4*i,triggers_V[i]);
+  }
+  // Wires data
+  EEPROM.write(34,toByte(trig_Wires));
+  // Finally the magic word to validate the process
+  EEPROM.put(0,101);
+  Serial.println("Config Written!");  
+}
+
+unsigned char toByte(bool b[8])
+{
+    unsigned char c = 0;
+    for (int i=0; i < 8; ++i)
+        if (b[i])
+            c |= 1 << i;
+    return c;
+}
+
+void fromByte(unsigned char c, bool b[8])
+{
+    for (int i=0; i < 8; ++i)
+        b[i] = (c & (1<<i)) != 0;
+}
+
