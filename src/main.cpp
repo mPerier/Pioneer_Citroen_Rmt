71b42841f0bdf1ce89acd44194b6591c69a6897f
mPerier <=>
2021-01-16 19:07:20 +0100

    now using const output voltages, many bugs resolved. System OK on test bench, need to test in reality

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 // ------------------- DEFINES ----------------------
 #define inWire1 A0
 #define inWire2 A1
-#define outWire 3
+#define outWire 10
 
-#define seuil_min 2.5 // The voltage under which no button is pressed
+#define seuil_min 1.5 // The voltage under which no button is pressed
 
 #define trig_inRes_Ohm 470 // The trigger resistance and voltage
 #define in_supplyVoltage 5
@@ -24,27 +24,23 @@ const unsigned int triggers_ohm[4] = {10,57,125,285};
 //  The loops that will use triggers_V will go from the last element to the first one.
 float triggers_V[4];
 
-const unsigned int actions_ohm[8] = {
-              // _________________________
-              // | OUT_V | FUNC | IN_OHM |
-              // |_______________________|
-  20000,      // | 4.000 | VOL- |   10   |
-  13500,      // | 5.106 | VOL+ |   57   |
-  7500,       // | 6.857 | NEXT |  125   |
-  10000,      // | 6.000 | PREV |  285   |
-  175,        // | 11.79 | SRCE |   10   |
-  5500,       // | 7.742 | DISP |   57   |
-  5500,       // | 7.742 | DISP |  125   |
-  2500        // | 9.600 | PLAY |  285   |
-  };          // |_______________________|
-
-// 
-unsigned int actions_V[8];
-
+const float actions_V[8] = {
+              // _______________________
+              // | OUT_K | FUNC | IN_R |
+              // |_____________________|
+  2.18,       // | 20    | VOL- |  10  |
+  1.86,       // | 13.5  | VOL+ |  57  |
+  1.43,       // | 7.5   | NEXT | 125  |
+  1.63,       // | 10    | PREV | 285  |
+  0.66,       // | 0.175 | SRCE |  10  |
+  1.24,       // | 5.5   | DISP |  57  |
+  1.24,       // | 5.5   | DISP | 125  |
+  0.89        // | 2.5   | PLAY | 285  |
+  };          // |_____________________|
 
 // ------------------ PROTOTYPES --------------------
 
-void sendPulse(unsigned int level);
+void sendPulse(float level_V);
 
 
 // -------------------- SETUP -----------------------
@@ -62,19 +58,13 @@ void setup() {
     triggers_V[i] = (float)in_supplyVoltage * ((float)trig_inRes_Ohm / ((float)triggers_ohm[i] + (float)trig_inRes_Ohm));
     Serial.println(triggers_V[i]);
   }
-  Serial.println("ACTIONS:");
-  // Init action_V
-  for (i=0;i<8;i++){
-    actions_V[i] = ((float)out_supplyVoltage * ((float)out_inRes_Ohm / ((float)actions_ohm[i] + (float)out_inRes_Ohm))/(float)5)*1024;  
-    Serial.println(actions_V[i]);
-  }
 }
-
 
 // -------------------- LOOP -----------------------
 void loop() {
   int i;
   float lastVoltage;
+  char msg[50];
 
   float readedVoltage1 = (analogRead(inWire1)/(float)1024)*5;
   float readedVoltage2 = (analogRead(inWire2)/(float)1024)*5;
@@ -89,7 +79,6 @@ void loop() {
       // We send the pulse corresponding to the action ID i
       sendPulse(actions_V[i]);
       triggered = true;
-      break;
     }
     // lastVoltage is used to define the lower limit of the range
     lastVoltage = triggers_V[i];
@@ -101,26 +90,20 @@ void loop() {
     for (i=3;i>=0;i--){
       if (readedVoltage2 > lastVoltage && readedVoltage2 < triggers_V[i]){
         sendPulse(actions_V[i+4]);
-        break;
       }
       lastVoltage = triggers_V[i];
     }
   }
-
 }
 
 
 // ------------------ FUNCTIONS --------------------
-void sendPulse(unsigned int level){
-  // The output is set to the given value
-  analogWrite(outWire,level);
+void sendPulse(float level_V){
+  // The output is set to the given value (8 bit resolution)
+  analogWrite(outWire,((level_V/(float)5)*(float)1024)/4);
   // While the button is maintained, the output stays high
   while (analogRead(inWire1) > seuil_min || analogRead(inWire1) > seuil_min){
     delay(50);
-    Serial.println("Waiting for button release...");
   }
-  analogWrite(outWire, 0);
-  Serial.print("--- Sended: [");
-  Serial.print(((float)level/(float)1024)*5);
-  Serial.println("]V ---");
+  digitalWrite(outWire,0);
 }
